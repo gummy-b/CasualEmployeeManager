36fe08ae21906a5eba7b39153073ed674fb5e273
Mogale M <thadhinsky@gmail.com>
2021-08-30 15:45:16 +0200

    Implement Roles SQL Repo

--- Data/Repos/Roles/RolesRepo.cs
+++ Data/Repos/Roles/RolesRepo.cs
@@ -1,17 +1,24 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CasualEmployee.API.Data.Repos.Roles
 {
-    public class RolesRepos : IRolesRepo
+    public class RolesRepo : IRolesRepo
     {
+        private readonly CEContext _con;
+
+        public RolesRepo(CEContext con)
+        {
+            _con = con;
+        }
         public IEnumerable<Models.Roles> GetAllRoles()
         {
-            throw new System.NotImplementedException();
+            return _con.Roles.ToList();
         }
 
         public Models.Roles GetRoles(int id)
         {
-            throw new System.NotImplementedException();
+            return _con.Roles.FirstOrDefault(r => r.Id == id);
         }
     }
 }
