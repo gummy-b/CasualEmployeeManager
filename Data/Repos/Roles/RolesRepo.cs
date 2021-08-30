8c5ea4a6989cc8dc030e9174bf38e6ac2f15fcde
Mogale M <thadhinsky@gmail.com>
2021-08-30 12:22:06 +0200

    Add SQL Roles Repo

---
+++ Data/Repos/Roles/RolesRepo.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace CasualEmployee.API.Data.Repos.Roles
+{
+    public class RolesRepos : IRolesRepo
+    {
+        public IEnumerable<Models.Roles> GetAllRoles()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Models.Roles GetRoles(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
