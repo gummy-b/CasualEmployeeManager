16fcd8a5a8cb8b7e2705d8760242867a4eb8f696
Mogale M <thadhinsky@gmail.com>
2021-08-30 15:23:12 +0200

    Setup Connection string

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -37,6 +38,8 @@ namespace CasualEmployee.API
 
             //Register Repo for DI
             services.AddScoped<IRolesRepo, R_MockRepo>();
+            //Register connection string
+            services.AddDbContext<CEContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("CEConnectionString")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
