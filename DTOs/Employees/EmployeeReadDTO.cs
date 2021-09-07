4f83036e818851936efcb1ace8df8c97b2570fdb
Mogale M <thadhinsky@gmail.com>
2021-09-07 17:54:51 +0200

    Refactor namespace

--- DTOs/Employees/EmployeeReadDTO.cs
+++ DTOs/Employees/EmployeeReadDTO.cs
@@ -2,7 +2,7 @@ using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace CasualEmployee.API.Models
+namespace CasualEmployee.API.DTOs.Employees
 {
     public class EmployeeReadDTO
     {
