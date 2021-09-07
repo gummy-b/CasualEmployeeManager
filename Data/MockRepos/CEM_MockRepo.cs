4b9a99e85f2016474a5f3e6ff2065ac97cb5dd43
Mogale M <thadhinsky@gmail.com>
2021-09-07 17:39:23 +0200

    refactor property name

--- Data/MockRepos/CEM_MockRepo.cs
+++ Data/MockRepos/CEM_MockRepo.cs
@@ -10,15 +10,15 @@ namespace CasualEmployee.API.Data.MockRepos
         {
             return new List<CEmployee>
             {
-                new CEmployee { Id = 1, Name = "Ted", Surname="Lasso", Role="Casual employee 1", ProfilePicture="img.png"},
-                new CEmployee { Id = 1, Name = "Tasha", Surname="Lasso", Role="Casual employee 3", ProfilePicture="img.jpg"},
-                new CEmployee { Id = 1, Name = "Tebo", Surname="Lasso", Role="Casual employee 2", ProfilePicture="img.png"}
+                new CEmployee { Id = 1, Name = "Ted", Surname="Lasso", RoleName="Casual employee 1", ProfilePicture="img.png"},
+                new CEmployee { Id = 1, Name = "Tasha", Surname="Lasso", RoleName="Casual employee 3", ProfilePicture="img.jpg"},
+                new CEmployee { Id = 1, Name = "Tebo", Surname="Lasso", RoleName="Casual employee 2", ProfilePicture="img.png"}
             };
         }
 
         public CEmployee GetEmployee(int id)
         {
-            return new CEmployee { Id = 1, Name = "Ted", Surname = "Lasso", Role = "Casual employee 1", ProfilePicture = "img.1" };
+            return new CEmployee { Id = 1, Name = "Ted", Surname = "Lasso", RoleName = "Casual employee 1", ProfilePicture = "img.1" };
         }
     }
 }
