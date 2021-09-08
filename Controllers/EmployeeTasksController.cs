aa769993ab354586d1890f1755239ea56ecbd7b7
Mogale M <thadhinsky@gmail.com>
2021-09-08 16:26:32 +0200

    Implement automapper to endpoints as well as put & post endpoints

--- Controllers/EmployeeTasksController.cs
+++ Controllers/EmployeeTasksController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using AutoMapper;
 using CasualEmployee.API.Data.Repos.Emp_Task;
+using CasualEmployee.API.DTOs.Employees.Tasks;
 using CasualEmployee.API.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,35 +21,53 @@ namespace CasualEmployee.API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<Employee_Task>> AllTasks()
+        public ActionResult<IEnumerable<TaskReadDTO>> AllTasks()
         {
             var taskItems = _repo.GetAllTasks();
 
-            return Ok(taskItems);
+            return Ok(_mapper.Map<TaskReadDTO>(taskItems));
         }
 
         [HttpGet("{id}", Name = "GetTask")]
-        public ActionResult<Employee_Task> GetTask(int id)
+        public ActionResult<TaskReadDTO> GetTask(int id)
         {
             var taskItem = _repo.GetTask(id);
 
             if (taskItem != null)
             {
-                return Ok(taskItem);
+                return Ok(_mapper.Map<TaskReadDTO>(taskItem));
             }
 
             return NotFound();
         }
 
         [HttpPost]
-        public ActionResult<Employee_Task> AddTask(Employee_Task task)
+        public ActionResult<TaskReadDTO> AddTask(CreateTaskDTO createTaskDTO)
         {
-            return Ok();
+            var taskModel = _mapper.Map<Employee_Task>(createTaskDTO);
+
+            _repo.CreateTask(taskModel);
+            _repo.SaveChanges();
+
+            var readTaskDTO = _mapper.Map<TaskReadDTO>(taskModel);
+
+            return CreatedAtRoute(nameof(GetTask), new { id = readTaskDTO.Id }, readTaskDTO);
         }
 
         [HttpPut("{id}")]
         public ActionResult UpdateTask(int id, Employee_Task task)
         {
+            var taskItem = _repo.GetTask(id);
+            if (taskItem == null)
+            {
+                return NotFound();
+            }
+
+            _mapper.Map(task, taskItem);
+
+            _repo.UpdateTask(taskItem);
+            _repo.SaveChanges();
+
             return NoContent();
         }
     }
