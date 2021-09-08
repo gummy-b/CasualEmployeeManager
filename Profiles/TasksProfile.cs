69c9d52cae6f332edc5b119de8ac00b7fc35ea9c
Mogale M <thadhinsky@gmail.com>
2021-09-08 16:14:22 +0200

    Add tbl attributes to domain model

---
+++ Profiles/TasksProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using CasualEmployee.API.Models;
+
+namespace CasualEmployee.API.Profiles
+{
+    public class TasksProfile : Profile
+    {
+        public TasksProfile()
+        {
+            CreateMap<Employee_Task, TaskReadDTO>();
+            CreateMap<CreateTaskDTO, Employee_Task>();
+            CreateMap<UpdateTaskDTO, Employee_Task>();
+        }
+    }
+}
